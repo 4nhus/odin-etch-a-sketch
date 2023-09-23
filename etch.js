8cea8c9c00a0e6199d2082e6b92808eb6e7ec807
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-09-23 23:57:51 +1000

    Add colour fill on grid square hover

--- etch.js
+++ etch.js
@@ -4,12 +4,19 @@ for (let i = 0; i < 16; i++) {
     const gridRow = document.createElement('div');
 
     for (let j = 0; j < 16; j++) {
-        const squareDiv = document.createElement('div');
-        squareDiv.classList.add('grid-square');
-        gridRow.appendChild(squareDiv);
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        gridRow.appendChild(gridSquare);
     }
 
     gridRow.classList.add('grid-row');
 
     gridContainer.appendChild(gridRow);
 }
+
+const gridSquares = document.querySelectorAll('.grid-square');
+gridSquares.forEach(gridSquare => {
+   gridSquare.addEventListener('mouseover', (e) => {
+       e.target.style.backgroundColor = 'darkgray';
+   })
+});
