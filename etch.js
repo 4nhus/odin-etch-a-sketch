f8eaabfdb439c55c825e19c57919b4cb7847101b
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-10-04 23:38:05 +1100

    Complete all tasks

--- etch.js
+++ etch.js
@@ -1,22 +1,56 @@
-const gridContainer = document.querySelector('.grid');
+const randomUpTo100 = () => Math.ceil(Math.random() * 100);
 
-for (let i = 0; i < 16; i++) {
-    const gridRow = document.createElement('div');
+function buildGrid(numSquaresPerRow) {
+    const gridContainer = document.querySelector('.grid');
+    gridContainer.remove();
 
-    for (let j = 0; j < 16; j++) {
-        const gridSquare = document.createElement('div');
-        gridSquare.classList.add('grid-square');
-        gridRow.appendChild(gridSquare);
+    const body = document.querySelector('body');
+    const newGridContainer = document.createElement('div');
+    newGridContainer.classList.add('grid');
+
+    for (let i = 0; i < numSquaresPerRow; i++) {
+        const gridRow = document.createElement('div');
+
+        for (let j = 0; j < numSquaresPerRow; j++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.classList.add('grid-square');
+            gridRow.appendChild(gridSquare);
+        }
+
+        gridRow.classList.add('grid-row');
+
+        newGridContainer.appendChild(gridRow);
     }
 
-    gridRow.classList.add('grid-row');
+    body.appendChild(newGridContainer);
 
-    gridContainer.appendChild(gridRow);
+    const gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach(gridSquare => {
+        gridSquare.addEventListener('mouseover', (e) => {
+            if (!e.target.classList.contains('hovered')) {
+                const r = randomUpTo100();
+                const g = randomUpTo100();
+                const b = randomUpTo100();
+
+                e.target.style.backgroundColor = `rgb(${r}% ${g}% ${b}% / 0.1)`;
+                e.target.classList.add('hovered');
+            } else if (e.target.style.backgroundColor.charAt(e.target.style.backgroundColor.lastIndexOf('.') - 1) === '0') {
+                const num = e.target.style.backgroundColor.charAt(e.target.style.backgroundColor.lastIndexOf('.') + 1) - '0';
+                if (num !== 9) {
+                    e.target.style.backgroundColor = e.target.style.backgroundColor.slice(0, e.target.style.backgroundColor.lastIndexOf('.') + 1).concat((num + 1) + ')');
+                } else {
+                    e.target.style.backgroundColor = e.target.style.backgroundColor.slice(0, e.target.style.backgroundColor.lastIndexOf('0')).concat('1)');
+                }
+            }
+        })
+    });
 }
 
-const gridSquares = document.querySelectorAll('.grid-square');
-gridSquares.forEach(gridSquare => {
-   gridSquare.addEventListener('mouseover', (e) => {
-       e.target.style.backgroundColor = 'darkgray';
-   })
+const button= document.querySelector('button');
+button.addEventListener('click', (e) => {
+   let numSides = parseInt(prompt("How many squares per row in the new grid? Enter a maximum up to 100."));
+   numSides = numSides > 100 ? 100 : numSides;
+   buildGrid(numSides);
 });
+
+buildGrid(16);
